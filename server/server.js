fcea733227110f98db02fd7d986a322629f6eccd
Flurry19 <sharonirenedewit@gmail.com>
2024-03-07 09:57:41 +0100

    chat backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
-
+import dotenv from "dotenv";
+import express from 'express';
 import { ChatOpenAI } from "@langchain/openai"
 
-console.log("hello world");
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.listen(process.env.EXPRESS_PORT, () =>  {
+    console.log(`Server running on port ${process.env.EXPRESS_PORT}`);
+})
 
 const model = new ChatOpenAI({
     azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
@@ -10,5 +18,37 @@ const model = new ChatOpenAI({
     azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
 })
 
-const question = await model.invoke("Tell a joke")
-console.log(question.content)
+    app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.get("/joke", async (req, res) =>{
+try{
+    const question = await model.invoke("Tell a joke")
+    console.log(question.content)
+    res.json({question: question.content})
+} catch (error){
+    console.error("error")
+}
+})
+
+app.post("/chat", async (req,res) => {
+    try{
+        const question = req.body.question;
+        console.log(question)
+        const answer = await model.invoke(question);
+        if (!question){
+            return res.status(400).json({error: 'No question has been asked'})
+        }
+        res.json({answer: answer.content})
+    } catch(error){
+        console.error("Can't get the answer")
+    }
+})
+
+
+
+// const prompt = 'Je bent een webshopbeheerder';
+
